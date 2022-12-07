32e9ce81e40e9e8cfc240a4dd815499f1d857614
Victor Rincón <victor.rincon@happeo.com>
2022-12-07 13:23:07 +0200

    upgrades Cypress to version 12

--- cypress.config.ts
+++ cypress.config.ts
@@ -14,5 +14,6 @@ export default defineConfig({
       return require("./cypress/plugins/index.js")(on, config);
     },
     baseUrl: "http://localhost:8080",
+    experimentalRunAllSpecs: true, 
   },
 });
